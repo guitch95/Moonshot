ee6eaf11f1fb7606e577929eeed26b4fa322829c
guitch95 <guitch95@gmail.com>
2026-02-04 12:23:56 +0100

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Guillaume Richard on 04/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
